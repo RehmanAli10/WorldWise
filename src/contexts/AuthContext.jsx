d90ad6f1866b77c08cf65e0205d85b823c464ec7
Rehman Ali <vrehmanali@gmail.com>
2024-02-04 21:35:05 +0500

    updated Auth context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -25,8 +25,8 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "rehman ali",
+  email: "rehman@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
