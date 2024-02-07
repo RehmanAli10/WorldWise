5f221ea527f4613ad35a11ac0d590923b1f629d4
Rehman Ali <vrehmanali@gmail.com>
2024-02-07 07:36:00 +0500

    Implemented lazy loading for improved performance, reducing bundle size from 536kb to 376.70kb

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,55 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import HomePage from "./pages/Homepage";
-import Pricing from "./pages/Pricing.jsx";
-import Product from "./pages/Product.jsx";
-import Login from "./pages/Login.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+
 import ProtectedRoutes from "./pages/ProtectedRoutes.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const Product = lazy(() => import("./pages/Product.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const AppLayout = lazy(() => import("./pages/AppLayout.jsx"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
+        <Suspense fallback={<SpinnerFullPage />}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
 
-            <Route
-              path="app"
-              element={
-                <ProtectedRoutes>
-                  <AppLayout />
-                </ProtectedRoutes>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoutes>
+                    <AppLayout />
+                  </ProtectedRoutes>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
 
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </CitiesProvider>
     </AuthProvider>
   );
